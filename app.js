f5874af61793b7c4d9fe064bcf6a6dce60cb8d7e
kmayer <kylemayer@x.local>
2021-05-03 16:32:54 -0700

    pretty much all setup and finalized in this commit

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
+import { checkForYes } from './utils.js';
+
+
 // import functions and grab DOM elements
+const button = document.getElementById('button');
+const resultsDiv = document.getElementById('results');
 
 // initialize state
 
 // set event listeners to update state and DOM
+
+button.addEventListener('click', ()=> {
+    alert('Welcome to the Philsophers Stone quiz');
+    const wantsQuiz = confirm('Do you want to take the quiz?');
+    if (wantsQuiz === false) {
+        // the word return STOPS any function.
+        return;
+    }
+    const firstName = prompt('What is your first name?');
+    const lastName = prompt('What is your last name?');
+
+    const answer1 = prompt('Was the Philsophers Stone useful for rejuvenation and for achieving immortality?');
+    
+    let score = 0;
+
+    if (checkForYes(answer1)) {
+        score++;
+    }
+
+    const answer2 = prompt('Can the holder of the stone turn wood into gold?');
+
+
+    if (!checkForYes(answer2)) {
+        score++;
+    }
+
+    const answer3 = prompt('Can the stone transmute common crystals into precious stones and diamond?');
+    
+
+    if (checkForYes(answer3)) {
+        score++;
+    }
+
+    const result = `Congratulations ${firstName} ${lastName}! You got ${score} out of 3!`;
+
+    resultsDiv.textContent = result;
+
+});
+
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,31 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { checkForYes } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('should take anything that starts with a y and return true', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = checkForYes('yes');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('should take anything that doesnt start with a y and return false', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = false;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = checkForYes('naaah');
 
     //Expect
     // Make assertions about what is expected versus the actual result
